e2c1f401a6a8a17825b6909d7852015e9a53d4fe
Bhuwan Pandit <bp7968h@gre.ac.uk>
2024-07-22 07:31:47 +0100

    Implement IntoIter

--- src/second.rs
+++ src/second.rs
@@ -7,6 +7,9 @@ struct Node<T>{
     next: Option<Box<Node<T>>>
 }
 
+pub struct IntoIter<T>(List<T>);
+
+
 impl<T> List<T> {
     pub fn new() -> Self {
         List {head: None}
@@ -38,6 +41,18 @@ impl<T> List<T> {
         self.head.as_mut().map(|node| {
             &mut node.elem
         })
+    }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T>{
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
     }
 }
 
@@ -98,5 +113,17 @@ mod test {
 
         assert_eq!(list.peek(), Some(&42));
         assert_eq!(list.pop(), Some(42));
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
+    
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
     }
 }
