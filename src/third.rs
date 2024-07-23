2b9a3a25faa231eb717fca88920bef4170b3419d
Bhuwan Pandit <bp7968h@gre.ac.uk>
2024-07-23 19:35:13 +0100

    Add Drop trait to  persistent immutable singly-linked list

--- src/third.rs
+++ src/third.rs
@@ -51,6 +51,19 @@ impl<'a, T> Iterator for Iter<'a, T> {
     }
 }
 
+impl<T> Drop for List<T>{
+    fn drop(&mut self) {
+        let mut head = self.head.take();
+        while let Some(node) = head {
+            if let Ok(mut node) = Rc::try_unwrap(node) {
+                head = node.next.take();
+            } else {
+                break;
+            }
+        }
+    }
+}
+
 
 #[cfg(test)]
 mod test {
@@ -82,7 +95,7 @@ mod test {
     #[test]
     fn iter() {
         let list = List::new().prepend(1).prepend(2).prepend(3);
-    
+
         let mut iter = list.iter();
         assert_eq!(iter.next(), Some(&3));
         assert_eq!(iter.next(), Some(&2));
